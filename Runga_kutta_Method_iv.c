683279e8d71df1b0ce00c0d180e09f6348272d6d
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-17 11:50:44 +0530

    runga-kutta-ivth order

--- Runga_kutta_Method_iv.c
+++ Runga_kutta_Method_iv.c
@@ -9,10 +9,17 @@ float f(float x,float y)
 int main()
 {
     float x0,y0,m1,m2,m3,m4,m,y,x,h,xn;
-    printf("Enter int x0: ");scanf("%f",&x0);
-    printf("Enter the y0: ");scanf("%f",&y0);
-    printf("Enter the xn: ");scanf("%f",&xn);
-    printf("Enter the h: ");scanf("%f",&h);
+    printf("Enter int x0: ");
+    scanf("%f",&x0);
+    
+    printf("Enter the y0: ");
+    scanf("%f",&y0);
+    
+    printf("Enter the xn: ");
+    scanf("%f",&xn);
+    
+    printf("Enter the h: ");
+    scanf("%f",&h);
 
     x=x0;
     y=y0;
@@ -24,8 +31,10 @@ int main()
         m3=f((x0+h/2.0),(y0+m2*h/2.0));
         m4=f((x0+h),(y0+m3*h));
         m=((m1+2*m2+2*m3+m4)/6);
-        y=y+m*h;
-        x=x+h;
+
+        x+=h;
+        y+=m*h;
+
         printf("%f\t%f\n",x,y);
     }
 }
