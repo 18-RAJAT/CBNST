65df34d00ce40239b86261f1de833e6df38b7c77
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-17 12:13:56 +0530

    runga-kutta-ivth order update

--- Runga_kutta_Method_iv.c
+++ Runga_kutta_Method_iv.c
@@ -1,40 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
-float f(float x,float y)
-{
-    float m;
-    m=(x-y)/(x+y);
-    return m;
-}
+
+#define take(x,y) (y*y-x)/(y*y+x)
+
 int main()
 {
-    float x0,y0,m1,m2,m3,m4,m,y,x,h,xn;
-    printf("Enter int x0: ");
+    int n;
+    float x0,y0,xn,yn,res,k1,k2,k3,k4,stepSize;
+
+    printf("Enter the initial conditions\n");
+
+    printf("Enter x0: ");
     scanf("%f",&x0);
-    
-    printf("Enter the y0: ");
+
+    printf("Enter y0: ");
     scanf("%f",&y0);
-    
-    printf("Enter the xn: ");
+
+    printf("Enter the calculating point: ");
     scanf("%f",&xn);
-    
-    printf("Enter the h: ");
-    scanf("%f",&h);
 
-    x=x0;
-    y=y0;
-    printf("\n\nX\t\tY\n");
-    while(x<xn)
+    printf("Enter the steps: ");
+    scanf("%d",&n);
+
+    stepSize=(xn-x0)/n;
+
+    for(int i=0;i<n;++i)
     {
-        m1=f(x0,y0);
-        m2=f((x0+h/2.0),(y0+m1*h/2.0));
-        m3=f((x0+h/2.0),(y0+m2*h/2.0));
-        m4=f((x0+h),(y0+m3*h));
-        m=((m1+2*m2+2*m3+m4)/6);
+        k1=stepSize*(take(x0,y0));
+        k2=stepSize*(take((x0+stepSize/2),(y0+k1/2)));
+        k3=stepSize*(take((x0+stepSize/2),(y0+k2/2)));
+        k4=stepSize*(take((x0+stepSize),(y0+k3)));
 
-        x+=h;
-        y+=m*h;
+        res=(k1+2*k2+2*k3+k4)/6;
 
-        printf("%f\t%f\n",x,y);
+        yn=res+y0;
+        printf("%0.4f\t%0.4f\t%0.4f\n",x0,y0,yn);
+        x0+=res;
+        y0=yn;
     }
+    printf("\nValue of y at x = %0.2f is %0.3f",xn, yn);
 }
