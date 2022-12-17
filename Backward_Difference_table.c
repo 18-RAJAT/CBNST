a2ab78c26625c246645bf91193a570963cc6def5
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-12-17 13:23:43 +0530

    Backward-difference-table

--- Backward_Difference_table.c
+++ Backward_Difference_table.c
@@ -18,23 +18,24 @@ int main()
         printf("b[%d]= ",i);
         scanf("%f",&b[i][0]);
     }
+    
     for(int i=1;i<n;++i)
     {
-        for(int j=n-1;j>i-1;--i)
+        for(int j=n-1;j>i-1;--j)
         {
             b[j][i]=b[j][i-1]-b[j-1][i-1];
         }
     }
-    // printf("Working");
-    printf("Difference Table");
+    printf("Difference Table\n");
     for(int i=0;i<n;++i)
     {
-        printf("%.3f",a[i]);
+        printf("\t %.3f",a[i]);
 
         for(int j=0;j<=i;++j)
         {
-            printf("%.3f",b[i][j]);
+            printf("\t %.3f",b[i][j]);
         }
         printf("\n");
     }
+    return 0;
 }
